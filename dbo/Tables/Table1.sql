628667e4b7d428499da7ddb0ed27c75c5cab39ab
MaraPrazzy <61703707+MaraPrazzy@users.noreply.github.com>
2021-01-25 19:43:42 -0500

    table1 removed column

--- dbo/Tables/Table1.sql
+++ dbo/Tables/Table1.sql
@@ -1,10 +1,11 @@
 ﻿CREATE TABLE [dbo].[Table1] (
-    [Id]            INT        NOT NULL,
-    [Nestle_Brand]  NCHAR (10) NULL,
-    [Spend]         INT        NULL,
-    [Rate]          INT        NULL,
-    [PlannedAmount] FLOAT (53) NULL,
+    [Id]           INT        NOT NULL,
+    [Nestle_Brand] NCHAR (10) NULL,
+    [Spend]        INT        NULL,
+    [Rate]         INT        NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
 
